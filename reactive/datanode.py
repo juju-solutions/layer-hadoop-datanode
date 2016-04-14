3abb08a4ddbaf0fe7582416612937a23dbe1ba46
Konstantin Boudnik <cos@boudnik.org>
2016-04-13 19:14:10 -0600

    Adding port open/close logic

--- reactive/datanode.py
+++ reactive/datanode.py
@@ -1,12 +1,13 @@
 from charms.reactive import when, when_not, set_state, remove_state
 from charms.layer.apache_bigtop_base import get_bigtop_base
-from charmhelpers.core import host
+from charmhelpers.core import host, hookenv
 
+datanode_port = '50075'
 @when_not('datanode.started')
 def start_datanode():
     bigtop = get_bigtop_base()
     host.service_start('hadoop-hdfs-datanode')
-    bigtop.open_ports('datanode')
+    hookenv.open_port(datanode_port) # TODO need to read the ports from layer.yaml
     set_state('datanode.started')
 
 
@@ -15,5 +16,5 @@ def start_datanode():
 def stop_datanode():
     bigtop = get_bigtop_base()
     host.service_stop('hadoop-hdfs-datanode')
-    bigtop.close_ports('datanode')
+    hookenv.close_port(datanode_port)  # TODO need to read the ports from layer.yaml
     remove_state('datanode.started')
