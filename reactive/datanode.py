84f30226cd86e7499fe58b4a4d4c6ffe28e7e0f1
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-27 18:16:48 -0500

    multiple updates to bigtop-datanode
    - use full layer prefix for states (apache-bigtop-datanode vs datanode)
    - send datanode spec when available so nn.ready can be set
    - handle absence of datanodes with status

--- reactive/datanode.py
+++ reactive/datanode.py
@@ -5,29 +5,42 @@ from charmhelpers.core import host, hookenv
 
 @when_not('namenode.joined')
 def blocked():
-    hookenv.status_set('blocked', 'waiting for namenode relation')
+    hookenv.status_set('blocked', 'missing required namenode relation')
 
 
-@when('namenode.joined', 'puppet.available')
-@when_not('datanode.installed')
-def install_hadoop(namenode):
-    '''Install only if the namenode has sent its FQDN.'''
+@when('puppet.available', 'namenode.joined')
+@when_not('apache-bigtop-datanode.installed')
+def install_datanode(namenode):
+    """Install if the namenode has sent its FQDN.
+
+    We only need the namenode FQDN to perform the datanode install, so poll for
+    namenodes() data whenever we have a namenode relation. This allows us to
+    install asap, even if 'namenode.ready' is not set yet.
+    """
     if namenode.namenodes():
         hookenv.status_set('maintenance', 'installing datanode')
         nn_host = namenode.namenodes()[0]
         bigtop = get_bigtop_base()
         hosts = {'namenode': nn_host}
         bigtop.install(hosts=hosts, roles='datanode')
-        bigtop.setup_hdfs
-        set_state('datanode.installed')
+        set_state('apache-bigtop-datanode.installed')
         hookenv.status_set('maintenance', 'datanode installed')
     else:
-        hookenv.status_set('waiting', 'waiting for namenode to become ready')
+        hookenv.status_set('waiting', 'waiting for namenode fqdn')
 
 
-@when('namenode.joined')
-@when('datanode.installed')
-@when_not('datanode.started')
+@when('apache-bigtop-datanode.installed', 'namenode.joined')
+@when_not('namenode.ready')
+def send_nn_spec(namenode):
+    """Send our datanode spec so the namenode can become ready."""
+    bigtop = get_bigtop_base()
+    # Send DN spec (must match NN spec for 'namenode.ready' to be set)
+    namenode.set_local_spec(bigtop.spec())
+    hookenv.status_set('waiting', 'waiting for namenode to become ready')
+
+
+@when('apache-bigtop-datanode.installed', 'namenode.ready')
+@when_not('apache-bigtop-datanode.started')
 def start_datanode(namenode):
     hookenv.status_set('maintenance', 'starting datanode')
     # NB: service should be started by install, but this may be handy in case
@@ -36,19 +49,24 @@ def start_datanode(namenode):
     host.service_restart('hadoop-hdfs-datanode')
     for port in get_layer_opts().exposed_ports('datanode'):
         hookenv.open_port(port)
-    set_state('datanode.started')
+
+    # Create a /user/ubuntu dir in HDFS (this is safe to run multiple times).
+    bigtop = get_bigtop_base()
+    bigtop.setup_hdfs()
+
+    set_state('apache-bigtop-datanode.started')
     hookenv.status_set('active', 'ready')
 
 
-@when('datanode.started')
-@when_not('namenode.joined')
+@when('apache-bigtop-datanode.started')
+@when_not('namenode.ready')
 def stop_datanode():
     hookenv.status_set('maintenance', 'stopping datanode')
     for port in get_layer_opts().exposed_ports('datanode'):
         hookenv.close_port(port)
     host.service_stop('hadoop-hdfs-datanode')
-    remove_state('datanode.started')
-    # Remove the installed state so we can re-configure the installation
+    remove_state('apache-bigtop-datanode.started')
+    # NB: Remove the installed state so we can re-configure the installation
     # if/when a new namenode comes along in the future.
-    remove_state('datanode.installed')
+    remove_state('apache-bigtop-datanode.installed')
     hookenv.status_set('maintenance', 'datanode stopped')
